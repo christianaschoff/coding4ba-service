b8c2f1669fcb2818b39047368116c12d7dfbcd97
christianaschoff <christian.aschoff@diw-sc.de>
2024-04-09 21:03:42 +0200

    add first controller operation

--- backendservice/src/main/java/de/bag/backendservice/controllers/calculusController.java
+++ backendservice/src/main/java/de/bag/backendservice/controllers/calculusController.java
@@ -1,4 +1,8 @@
 package de.bag.backendservice.controllers;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,5 +11,10 @@ public class calculusController {
     public calculusController() {        
         super();
     }
-
+ 
+    @GetMapping("/calculus/{eingabe}")
+    public ResponseEntity<Integer> powerOfTwo(@PathVariable("eingabe") Integer zahl) {
+        Integer ergebnis = zahl * zahl;
+        return new ResponseEntity<Integer>(ergebnis,  HttpStatusCode.valueOf(200));
+    }
 }
