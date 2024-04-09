eee0c0188b9f781d54d5c57737eceea2f567cb6e
christianaschoff <christian.aschoff@diw-sc.de>
2024-04-09 22:29:48 +0200

    clear solution

--- backendservice/src/main/java/de/bag/backendservice/models/calculusModel.java
+++ backendservice/src/main/java/de/bag/backendservice/models/calculusModel.java
@@ -1,14 +1,9 @@
 package de.bag.backendservice.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-public class calculusModel {
-    double zahl1;
-    String operant;
-    double zahl2;
+public class calculusModel {    
 }
