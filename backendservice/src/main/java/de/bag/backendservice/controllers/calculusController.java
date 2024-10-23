7ee11d99ba8659fad1e0f3005a699ecb9e688620
christianaschoff <christian.aschoff@diw-sc.de>
2024-10-23 22:56:01 +0200

    cors exc

--- backendservice/src/main/java/de/bag/backendservice/controllers/calculusController.java
+++ backendservice/src/main/java/de/bag/backendservice/controllers/calculusController.java
@@ -1,6 +1,7 @@
 package de.bag.backendservice.controllers;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import de.bag.backendservice.logic.CalculusLogic;
 import de.bag.backendservice.models.CalculusModel;
 
 @RestController
+@CrossOrigin
 public class CalculusController {
     
     CalculusLogic calculuslogic;
