7c5c34c1d030452805de58340b9c8ece100be537
christianaschoff <christian.aschoff@diw-sc.de>
2024-11-07 18:45:28 +0100

    fix angular update bug

--- frontend/src/app/calculus/calculus.component.ts
+++ frontend/src/app/calculus/calculus.component.ts
@@ -4,7 +4,8 @@ import { CalculusModel } from '../models/calculus-model';
 @Component({
   selector: 'app-calculus',
   templateUrl: './calculus.component.html',
-  styleUrls: ['./calculus.component.scss']
+  styleUrls: ['./calculus.component.scss'],
+  standalone: false
 })
 export class CalculusComponent {
   model: CalculusModel = {};
